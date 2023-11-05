c05dec93a4ea96fabee4fe60d289735a853c2df7
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-05 11:48:19 +0530

    sorting complete

--- src/page/Services.jsx
+++ src/page/Services.jsx
@@ -3,12 +3,26 @@ import Container from "../components/ui/Container";
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "../hooks/useAxios";
 import ServiceCard from "../components/ServiceCard";
+import { useState } from "react";
+import { capitalizeWords } from "../utils/Capitalize";
+
+const categories = [
+  "landscaping",
+  "home-improvement",
+  "home-services",
+  "office-services",
+];
 
 const Services = () => {
   const axios = useAxios();
-
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  // console.log(category);
+  // console.log(price);
   const getServices = async () => {
-    const res = await axios.get("/services");
+    const res = await axios.get(
+      `/services?sortField=price&sortOrder=${price}&category=${category}`
+    );
     return res;
   };
 
@@ -20,7 +34,7 @@ const Services = () => {
     isLoading,
     isError,
   } = useQuery({
-    queryKey: ["service"],
+    queryKey: ["service", price, category], //eta somehow dependency hisabe kaj kore price/category change hole refetch hbe
     queryFn: getServices, //data jeta fetch kore anbe
     /*  queryFn:()=>{
       return axios.get("/services");
@@ -28,13 +42,14 @@ const Services = () => {
   });
   // console.log(data.data);
 
-  if (isLoading) {
+  /* if (isLoading) {
     return <p>Loading ....</p>;
-  }
+  } */
   if (isError) {
     return <p>Error ....{error}</p>;
   }
   // console.log(status);
+  // console.log(services?.data?.total);
   return (
     <>
       <Container className="mt-10">
@@ -45,13 +60,60 @@ const Services = () => {
           deleniti atque explicabo esse.
         </Header>
       </Container>
+      <Container>
+        <div className="my-12 flex justify-end items-center border-2 border-primary rounded-2xl p-5 gap-5">
+          <h1 className="flex-1 text-xl font-semibold">
+            Over {services?.data?.total} services to choose from
+          </h1>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <select
+              name=""
+              id=""
+              className="input input-bordered"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option disabled selected>
+                Choose One
+              </option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {capitalizeWords(category)}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <select
+              name=""
+              id=""
+              className="input input-bordered"
+              onChange={(e) => setPrice(e.target.value)}
+            >
+              <option disabled selected>
+                Choose One
+              </option>
+              <option value="asc">From Low to High</option>
+              <option value="desc">From High to Low</option>
+            </select>
+          </div>
+        </div>
+      </Container>
+
       <Container className="mb-64">
+        {isLoading ? <p>Loading....</p> :
         <div className="grid grid-cols-3 gap-10">
           {/* Service Cards goes here */}
           {services?.data?.result.map((item) => (
             <ServiceCard key={item._id} service={item}></ServiceCard>
           ))}
-        </div>
+        </div>}
       </Container>
       <Container className="mb-16 -mt-48 flex justify-end">
         <div className="join border-2 border-primary">
